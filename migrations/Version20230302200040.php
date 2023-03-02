09b7fef73909642af17855ae6a510da587a9e0e3
guillaumeRequena <g.requena@laposte.net>
2023-03-02 21:10:59 +0100

    remove comments

--- migrations/Version20230302200040.php
+++ migrations/Version20230302200040.php
@@ -7,9 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
+
 final class Version20230302200040 extends AbstractMigration
 {
     public function getDescription(): string
@@ -19,13 +17,11 @@ final class Version20230302200040 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // ALTER TABLE user ADD is_verified TINYINT(1) NOT NULL
         $this->addSql('ALTER TABLE user CHANGE account_enabled is_verified TINYINT(1) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        // ALTER TABLE `user` DROP is_verified
         $this->addSql('ALTER TABLE `user` CHANGE is_verified account_enabled TINYINT(1) NOT NULL');
     }
 }
