c913cb352c0691895594b187e267cb39a7fe981b
guillaumeRequena <g.requena@laposte.net>
2023-03-29 20:58:46 +0200

    create show page and start css

---
+++ src/Controller/TrickController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Trick;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TrickController extends AbstractController
+{
+    #[Route('/trick/show/{id}', name: 'app_trick', requirements: ['id' => '\d+'])]
+    public function index(Trick $trick): Response
+    {
+        if(!$trick){
+            $this->addFlash('danger', 'Trick page not found.');
+            return $this->redirectToRoute('app_home');
+        }
+
+        $allImages = $trick->getImages();
+        $mainImage = $allImages[0];
+        $otherImages = [];
+
+        for($i = 1; $i < count($allImages); ++$i){
+            $otherImages[] = $allImages[$i];
+        }
+
+        return $this->render('trick/index.html.twig', [
+            'trick' => $trick,
+            'mainImage' => $mainImage,
+            'otherImages' => $otherImages
+        ]);
+    }
+}
