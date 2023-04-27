fd21dcb379e6c4404c6bf2bfea03bf8006576d80
guillaumeRequena <g.requena@laposte.net>
2023-04-27 20:50:48 +0200

    remove useless spaces from fixtures text

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -41,94 +41,63 @@ class AppFixtures extends Fixture
         // Grab
         [
             'title' => 'Mute',
-            'description' => '
-                    In this trick, when a player is in the air, he has to bend down on the board and grab the toe edge of the 
-                    board between his two legs. He also has to grab the the middle edge of the board using his front hand at that moment.
-                ',
+            'description' => 'In this trick, when a player is in the air, he has to bend down on the board and grab the toe edge of the board between his two legs. He also has to grab the the middle edge of the board using his front hand at that moment.',
             'photos' => ['19.jpg', '20.jpg', '21.jpg'],
             'videos' => ['https://www.youtube.com/embed/jm19nEvmZgM', 'https://www.youtube.com/embed/k6aOWf0LDcQ']
         ],
         [
             'title' => 'sad',
-            'description' => '
-                    Quite similar to a nosebone but you grab with the front hand and on the heelside while the nose of the board points 
-                    towards the ground.
-                ',
+            'description' => 'Quite similar to a nosebone but you grab with the front hand and on the heelside while the nose of the board points towards the ground.',
             'photos' => ['24.jpg'],
             'videos' => ['https://www.youtube.com/embed/KEdFwJ4SWq4']
         ],
         [
             'title' => 'indy',
-            'description' => '
-                    An Indy grab, also known as an Indy air, is an aerial skateboarding, snowboarding and kitesurfing trick during which 
-                    the rider grabs their back hand on the middle of their board, between their feet, on the side of the board where their 
-                    toes are pointing, while turning backside.
-                ',
+            'description' => 'An Indy grab, also known as an Indy air, is an aerial skateboarding, snowboarding and kitesurfing trick during which the rider grabs their back hand on the middle of their board, between their feet, on the side of the board where their toes are pointing, while turning backside.',
             'photos' => ['1.png', '2.jpg', '3.jpg'],
             'videos' => ['https://www.youtube.com/embed/iKkhKekZNQ8']
         ],
         // Rotation
         [
             'title' => '180',
-            'description' => '
-                    A 180 is essentially spinning your snowboard, in the air, 180 degrees. You will start out facing one way down the 
-                    mountain and end up facing the other way. Before learning how to do a 180 you should already be comfortable with 
-                    ollies and with riding switch.
-                ',
+            'description' => 'A 180 is essentially spinning your snowboard, in the air, 180 degrees. You will start out facing one way down the mountain and end up facing the other way. Before learning how to do a 180 you should already be comfortable with ollies and with riding switch.',
             'photos' => ['12.png', '13.jpg'],
             'videos' => ['https://www.youtube.com/embed/ATMiAVTLsuc']
         ],
         [
             'title' => '360',
-            'description' => '
-                    A frontside 360 is when you leave the slope and rotate in the air 360 degrees before hitting the ground again.
-                ',
+            'description' => 'A frontside 360 is when you leave the slope and rotate in the air 360 degrees before hitting the ground again.',
             'photos' => ['4.jpg', '5.jpg', '6.jpg'],
             'videos' => ['https://www.youtube.com/embed/JJy39dO_PPE']
         ],
         [
             'title' => '1080',
-            'description' => '
-                    A 1080 consists of three full rotations in the air.
-                ',
+            'description' => 'A 1080 consists of three full rotations in the air.',
             'photos' => ['7.jpg', '8.png'],
             'videos' => ['https://www.youtube.com/embed/3XxfClLqjg4']
         ],
         // Old school
         [
             'title' => 'japan air',
-            'description' => '
-                    “Japan Air” is the name of a trick in which the airborne athlete takes his front hand, reaches down over his front 
-                    leg to grab the edge of his snowboard nearest to his toes—the “toe edge”—and then pulls the board behind him.
-                ',
+            'description' => '“Japan Air” is the name of a trick in which the airborne athlete takes his front hand, reaches down over his front leg to grab the edge of his snowboard nearest to his toes—the “toe edge”—and then pulls the board behind him.',
             'photos' => ['14.jpg', '15.png'],
             'videos' => ['https://www.youtube.com/embed/jH76540wSqU']
         ],
         [
             'title' => 'rocket air',
-            'description' => '
-                    Achieved when rider takes both hands and grabs the nose of the snowboard. Rusty Trombone - Combination of a 
-                    roast beef and nose grab done at the same time.
-                ',
+            'description' => 'Achieved when rider takes both hands and grabs the nose of the snowboard. Rusty Trombone - Combination of a roast beef and nose grab done at the same time.',
             'photos' => ['22.jpg', '23.png'],
             'videos' => ['https://www.youtube.com/embed/nom7QBoGh5w']
         ],
         [
             'title' => 'Backside Air',
-            'description' => '
-                    A trick performed on the backside wall of the halfpipe where the athlete grabs the heel edge of the board with 
-                    the front hand. Backside Handplant: A maneuver where the rider places either both hands or the rear hand on 
-                    the lip of the halfpipe and rotates 180 degrees in the backside direction.    
-                ',
+            'description' => 'A trick performed on the backside wall of the halfpipe where the athlete grabs the heel edge of the board with the front hand. Backside Handplant: A maneuver where the rider places either both hands or the rear hand on the lip of the halfpipe and rotates 180 degrees in the backside direction.',
             'photos' => ['9.jpg', '10.png', '11.png'],
             'videos' => ['https://www.youtube.com/embed/_CN_yyEn78M']
         ],
         [
             'title' => 'Method Air',
-            'description' => '
-                    A trick where the boarder grabs the heel edge of the board with their front hand, between their feet, and then 
-                    pulls the board towards their back, while arching their back and bending knees.
-                ',
+            'description' => 'A trick where the boarder grabs the heel edge of the board with their front hand, between their feet, and then pulls the board towards their back, while arching their back and bending knees.',
             'photos' => ['16.png', '17.png', '18.jpg'],
             'videos' => ['https://www.youtube.com/embed/qMsN26DBLVo']
         ]
