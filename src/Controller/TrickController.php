b04262bb5e0fadb703ac0c5625b41947eacc3fac
guillaumeRequena <g.requena@laposte.net>
2023-05-05 13:27:26 +0200

    add video by url works

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -6,16 +6,17 @@ use DateTime;
 use App\Entity\Trick;
 use DateTimeImmutable;
 use App\Entity\Comment;
-use App\Form\CommentFormType;
 use App\Form\TrickFormType;
+use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\String\Slugger\AsciiSlugger;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\String\Slugger\AsciiSlugger;
 
 class TrickController extends AbstractController
 {
@@ -168,64 +169,64 @@ class TrickController extends AbstractController
         name: 'app_trick_remove_image', 
         requirements: ['slug' => '[a-z0-9][a-z0-9-]{0,}[a-z0-9]', 'image' => '\d+\.{1}(jpg|jpeg|png)']
     )]
-    public function removeImage(Trick $trick, string $image): Response
+    public function removeImage(Trick $trick, string $image): JsonResponse
     {
         if(!$trick){
             $this->addFlash('danger', 'Trick not found');
-            return new Response();
+            return new JsonResponse(null, 404);
         }
 
         if(!in_array($image, $trick->getImages())){
             $this->addFlash('danger', 'This trick does not have this image');
-            return new Response();
+            return new JsonResponse(null, 404);
         }
 
         $trick->removeImage($image);
         $this->em->flush();
 
-        return new Response();
+        return new JsonResponse();
     }
 
     #[Route('/trick/edit/{slug}/main-image/{image}', 
         name: 'app_trick_main_image', 
         requirements: ['slug' => '[a-z0-9][a-z0-9-]{0,}[a-z0-9]', 'image' => '\d+\.{1}(jpg|jpeg|png)']
     )]
-    public function setMainImage(Trick $trick, string $image): Response
-    {
+    public function setMainImage(Trick $trick, string $image): JsonResponse
+    {        
         if(!$trick){
             $this->addFlash('danger', 'Trick not found');
-            return new Response();
+            return new JsonResponse(null, 404);
         }
 
         $trick->setMainImage($image);
 
         $this->em->flush();
 
-        return new Response();
+        return new JsonResponse();
     }
 
 
     #[Route('/trick/edit/{slug}/remove-video/{videoIndex}', 
-        name: 'app_trick_remove_image',
+        name: 'app_trick_remove_video',
         requirements: ['slug' => '[a-z0-9][a-z0-9-]{0,}[a-z0-9]', 'videoIndex' => '\d+']
     )]
-    public function removeVideo(Trick $trick, int $videoIndex): Response
+    public function removeVideo(Trick $trick, int $videoIndex): JsonResponse
     {
         if(!$trick){
             $this->addFlash('danger', 'Trick not found');
-            return new Response();
+            return new JsonResponse(null, 404);
         }
 
         $video = $trick->getVideos()[$videoIndex];
 
         if(!$video){
             $this->addFlash('danger', 'Video not found');
-            return new Response();
+            return new JsonResponse(null, 404);
         }
 
         $trick->removeVideo($video);
         $this->em->flush();
 
-        return new Response();
+        return new JsonResponse();
     }
 }
